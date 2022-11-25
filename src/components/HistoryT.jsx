718f96b04764262c17b64c869312289582717f2c
rasheed98-dev <rasheedwathiq11@gmail.com>
2022-11-26 00:58:43 +0300

    solved

---
+++ src/components/HistoryT.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+// deleteRow
+
+export default function HistoryT(props) {
+   return(<>
+   <div class="flex flex-col">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <table class="min-w-full">
+          <thead class="bg-white border-b">
+            <tr>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                #
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Date
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Report
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                prescription
+              </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                Delete history
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.history.map(h=>{ return(<tr class="bg-gray-100 border-b">
+               <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
+               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                 {h.date}
+               </td>
+               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                 {h.report} 
+               </td>
+               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                {h.prescription.map(p=><p>{p}</p>)}
+               </td>
+
+               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+               <button onClick={()=>props.deleteRow(h._id)} type="button" class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out">Danger</button>
+               </td>
+             </tr>)
+               
+            })}
+            
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+   
+   </>)
+        
+   
+}
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 import ThemeContext from "../contexts/ThemeContext";
 import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
 export const Sidebar = ()=>{
     const context = useContext(ThemeContext)
     const color = context.settings.themeValue.background;
+    const authContext = useContext(AuthContext)
+    const {logout} = authContext;
     return (<div className={`flex flex-col h-screen p-3 shadow w-60 ${color}`}>
     <div className="space-y-3">
         <div className="flex items-center">
@@ -30,6 +34,11 @@ export const Sidebar = ()=>{
                     to={'history'}>
                             History
                         </NavLink>
+                </li>
+
+                <li className="rounded-sm  h-8">
+                <button type="button" onClick={logout} class="inline-block px-6 py-2.5 bg-gray-200 text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-300 hover:shadow-lg focus:bg-gray-300 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">Logout</button>
+                {/* <button onClick={logout}>Logout</button> */}
                 </li>
                 
             </ul>
